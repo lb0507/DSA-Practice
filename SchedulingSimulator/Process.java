d4e06b83780d6013936b89b38a209f591f97d872
Kaley Bardwell <lexiebardwell0507@gmail.com>
2025-02-21 18:40:15 -0600

    Uploaded SchedulingSim

---
+++ SchedulingSimulator/Process.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author lexie
+ */
+public class Process {
+    
+        private int arrive;
+        private int priority;
+        private int cpu_time;
+        private int name;
+        private int end;
+        private int q_tracker;
+        
+        public void set_arrive(int x){
+            this.arrive = x;
+        }
+        public void set_priority(int x){
+            this.priority = x;
+        }
+        public void set_cpu_time(int x){
+            this.cpu_time = x;
+        }
+        public void set_name(int x){
+            this.name = x;
+        }
+        public void set_end(int x){
+            this.end = x;
+        }
+        public void set_q_tracker(int x) {
+        this.q_tracker = x;
+        }
+        public int get_arrive(){
+            return arrive;
+        }
+        public int get_priority(){
+            return priority;
+        }
+        public int get_cpu_time(){
+            return cpu_time;
+        }
+        public int get_name(){
+            return name;
+        }
+        public int get_end(){
+            return end;
+        }
+        public int get_q_tracker(){
+            return q_tracker;
+        }
+
+}
